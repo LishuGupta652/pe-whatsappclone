319357b7acebc74d1374597347734246570502b9
LishuGupta652 <lishugupta652@gmail.com>
2020-08-28 05:10:25 +0000

    basic

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAxfyKawcVQd7mLh9-dugsOJxzXzeVvehE",
+  authDomain: "whatsappcosmos.firebaseapp.com",
+  databaseURL: "https://whatsappcosmos.firebaseio.com",
+  projectId: "whatsappcosmos",
+  storageBucket: "whatsappcosmos.appspot.com",
+  messagingSenderId: "104437889315",
+  appId: "1:104437889315:web:6c4183ae4c8d96c009b496"
+};
